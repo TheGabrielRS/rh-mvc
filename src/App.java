9d8430b30c5bcc2d0c03561a2df6eb5b07eb6660
Gabriel Ramos <gabrielsa@ufcspa.edu.br>
2020-10-24 20:29:40 +0000

    incialização do app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import br.edu.ufcspa.view.AppView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        AppView app = new AppView();
+
+        app.iniciar();
     }
 }
